1685fee4e852718672fa822418dbab8fb5eb2c92
Timmyeriksson <timmyerikericsson@gmail.com>
2017-12-27 13:54:27 +0100

    NU KAN MAN ÄNDRA SPRÅK

---
+++ Dating/Controllers/LanguageController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dating.Controllers
+{
+    public class LanguageController : Controller
+    {
+        // GET: Language
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Change(string Language)
+        {
+            if (Language != null)
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(Language);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(Language);
+            }
+
+            HttpCookie cookie = new HttpCookie("Language");
+            cookie.Value = Language;
+            Response.Cookies.Add(cookie);
+
+            return View("Index");
+        }
+    }
+}
